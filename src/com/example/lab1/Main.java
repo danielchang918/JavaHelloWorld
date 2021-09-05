43bc1383130229d3c02be2cae37ad41e1a0d1ee2
danie <danie@172.17.151.71>
2021-09-05 22:16:42 +0900

    Lab1 comments added

--- src/com/example/lab1/Main.java
+++ src/com/example/lab1/Main.java
@@ -2,13 +2,17 @@ package com.example.lab1;
 import java.util.Scanner;
 
 public class Main {
-
+	
+	/**
+	 * This is the main method of lab1
+	 * @author Chang Hyun Woo
+	 */
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		System.out.println("Hello World!!!\n");
-		System.out.print("이름 입력 : ");
+		Scanner s = new Scanner(System.in); //declares scanner
+		System.out.println("Hello World!!!\n"); 
+		System.out.print("이름 입력 : "); //Input name
 		String name = s.next();
-		System.out.println("[" + name + "]님 환영합니다~!");
+		System.out.println("[" + name + "]님 환영합니다~!"); //greeting message with the input name
 	}
 
 }
